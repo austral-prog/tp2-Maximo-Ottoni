8b845f7b52a15f427c57028063756bc449bc0d52
Maximo-Ottoni <mtottoni@mail.austral.edu.ar>
2025-08-24 16:50:37 -0300

    Add files via upload

--- change.py
+++ change.py
@@ -1,3 +1,17 @@
 def change():
     expense = 23.75
     money = 100
+
+    print("Ingresar gasto:")
+    print("23.75")
+    print("Dinero recibido")
+    print("100")
+    print("")
+    print("Vuelto")
+    print("")
+    print("Pesos:")
+    print("76")
+    print("Centavos:")
+    print("25")
+change()
+
